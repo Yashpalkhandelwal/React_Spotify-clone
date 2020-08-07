d62fbf4d56f277c5615ef87d194c5b9a5a082d49
Yashpalkhandelwal <yashkumar1235pal@gmail.com>
2020-08-07 17:11:49 +0530

    Calling data of user from spotify api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 import './App.css';
 import Login from "./Login"
+import { getTokenFromResponse } from "./spotify"; 
+
+
+const s = new SpotifyWebApi();
 
 function App() {
+  const [{ token }, dispatch] = useStateValue();
+
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      s.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      s.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+    }
+    }, [token, dispatch]);
+
+
+
   return (
     <div className="app">
      
